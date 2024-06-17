d3705287ff087eba1aa90bc16bafc91dcd0b1ed8
dbernhard <dbernhard@ethz.ch>
2024-06-17 16:56:11 +0200

    Move enums to their own file

--- crawler/browser.py
+++ crawler/browser.py
@@ -44,39 +44,11 @@ from seleniumwire import webdriver
 from seleniumwire.request import Request, Response
 from seleniumwire.utils import decode
 
+from enums import PageState, CookieTuple
 
 logger = logging.getLogger("browser-cbcc")
 
 FuncT = TypeVar("FuncT", bound=Callable[..., Any])
-
-
-class PageState(Enum):
-    """
-    Enum to represent state of index page
-    """
-
-    # When the domain name cannot be resolved
-    DNS_ERROR = 15
-
-    # Everything OK
-    OK = 0
-    REDIRECT = 2
-    TIMEOUT = 3
-    BAD_CONTENT_TYPE = 11
-    HTTP_ERROR = 12
-    TCP_ERROR = 13
-    UNKNOWN_ERROR = 14
-
-
-class CookieTuple(NamedTuple):
-    name: str
-    value: str
-    path: str
-    domain: str
-    secure: bool
-    http_only: bool
-    expiry: int
-    same_site: str
 
 
 def post_load_routine(func: FuncT, browser_init: Optional["Browser"] = None) -> FuncT:
