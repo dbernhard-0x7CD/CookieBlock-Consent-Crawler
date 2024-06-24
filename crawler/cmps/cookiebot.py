210f09b2006c5aef4415e29a42ebb6a173d1f5f9
dbernhard <dbernhard@ethz.ch>
2024-06-24 13:56:56 +0200

    Comment out call to store cookie for now

--- crawler/cmps/cookiebot.py
+++ crawler/cmps/cookiebot.py
@@ -103,7 +103,9 @@ def internal_cookiebot_scrape(url: str, browser_id: int, visit_id: int, webdrive
 
             for c in cookies:
                 cookie_count += 1
-                send_cookiedat_to_db(c[0], c[1], cat_id, cat_name, browser_id, visit_id, c[2], c[3], c[4], c[5])
+
+                # TODO
+                # send_cookiedat_to_db(c[0], c[1], cat_id, cat_name, browser_id, visit_id, c[2], c[3], c[4], c[5])
 
     # format of the cookiebot data should be uniform, but in case this happens
     # to be violated, this try-except block catches it
