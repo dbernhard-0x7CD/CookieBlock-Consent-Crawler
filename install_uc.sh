28182cf1985aad6f024c744cc6058206a664d7c3
David Bernhard <dbernhard@ethz.ch>
2024-10-30 12:26:53 +0100

    Update to latest selenium version which includes fix to not wait indefinitely on a frozen chromedriver instance. Also update to chromium version 128

--- install_uc.sh
+++ install_uc.sh
@@ -13,13 +13,13 @@ mkdir -p ${CRAWLER_CHROME_DRIVER_PATH}
 mkdir -p ${CRAWLER_CHROME_PATH}
 mkdir -p ${CRAWLER_CHROME_PROFILE_PATH}
 
-# Chrome version 126; Also change version in run_consent_crawl.py if upgrading
+# Chrome version 128; Also change version in run_consent_crawl.py if upgrading
 cd /tmp/ || exit
-wget -q -O chrome-linux.zip 'https://www.googleapis.com/download/storage/v1/b/chromium-browser-snapshots/o/Linux_x64%2F1300319%2Fchrome-linux.zip?generation=1715640415104812&alt=media'
+wget -q -O chrome-linux.zip 'https://www.googleapis.com/download/storage/v1/b/chromium-browser-snapshots/o/Linux_x64%2F1331481%2Fchrome-linux.zip?generation=1721701707211374&alt=media'
 unzip chrome-linux.zip
 mv chrome-linux/* "${CRAWLER_CHROME_PATH}"
 
-wget -q -O chromedriver.zip 'https://www.googleapis.com/download/storage/v1/b/chromium-browser-snapshots/o/Linux_x64%2F1300319%2Fchromedriver_linux64.zip?generation=1715640418543191&alt=media'
+wget -q -O chromedriver.zip 'https://www.googleapis.com/download/storage/v1/b/chromium-browser-snapshots/o/Linux_x64%2F1331481%2Fchromedriver_linux64.zip?generation=1721701710498777&alt=media'
 
 unzip chromedriver.zip
 mv chromedriver_linux64/chromedriver "${CRAWLER_CHROME_DRIVER_PATH}"
