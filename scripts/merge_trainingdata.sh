143a99d193459b36da197daab5b349dc9a6c8818
David Bernhard <dbernhard@ethz.ch>
2024-11-21 21:05:17 +0100

    Add example to merge_trainingdata.sh for clarity

--- scripts/merge_trainingdata.sh
+++ scripts/merge_trainingdata.sh
@@ -3,6 +3,8 @@
 # Script used to merge all json files given via arguments.
 # Remove the -c to get pretty json
 
+# Example ./scripts/merge_trainingdata.sh crawl_data_A.json crawl_data_B.json crawl_data_C.json > merged_trainingdata.json
+
 jq -cs add $@
 
 
