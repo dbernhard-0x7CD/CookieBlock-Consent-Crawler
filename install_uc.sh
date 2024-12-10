b007e6e6899e2773fc1ebf4e9f6324d4b9c100d1
David Bernhard <dbernhard@ethz.ch>
2024-12-10 09:28:41 +0100

    Update to latest selenium and chromium 130

--- install_uc.sh
+++ install_uc.sh
@@ -13,13 +13,13 @@ mkdir -p ${CRAWLER_CHROME_DRIVER_PATH}
 mkdir -p ${CRAWLER_CHROME_PATH}
 mkdir -p ${CRAWLER_CHROME_PROFILE_PATH}
 
-# Chrome version 128; Also change version in run_consent_crawl.py if upgrading
+# Chrome version 130; Also change version in run_consent_crawl.py if upgrading
 cd /tmp/ || exit
-wget -q -O chrome-linux.zip 'https://www.googleapis.com/download/storage/v1/b/chromium-browser-snapshots/o/Linux_x64%2F1331481%2Fchrome-linux.zip?generation=1721701707211374&alt=media'
+wget -q -O chrome-linux.zip 'https://www.googleapis.com/download/storage/v1/b/chromium-browser-snapshots/o/Linux_x64%2F1356007%2Fchrome-linux.zip?generation=1726513315891763&alt=media'
 unzip chrome-linux.zip
 mv chrome-linux/* "${CRAWLER_CHROME_PATH}"
 
-wget -q -O chromedriver.zip 'https://www.googleapis.com/download/storage/v1/b/chromium-browser-snapshots/o/Linux_x64%2F1331481%2Fchromedriver_linux64.zip?generation=1721701710498777&alt=media'
+wget -q -O chromedriver.zip 'https://www.googleapis.com/download/storage/v1/b/chromium-browser-snapshots/o/Linux_x64%2F1356007%2Fchromedriver_linux64.zip?generation=1726513319019044&alt=media'
 
 unzip chromedriver.zip
 mv chromedriver_linux64/chromedriver "${CRAWLER_CHROME_DRIVER_PATH}"
