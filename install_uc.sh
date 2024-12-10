239aa18e42d7bc277295b069161ba5235730148c
David Bernhard <dbernhard@ethz.ch>
2024-12-10 10:06:01 +0100

    install_uc.sh: Use rsync to not fail if chrome was installed before and update installation completely

--- install_uc.sh
+++ install_uc.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # Installs chrome to the current directory or the first argument.
+# Requires wget and rsync to be installed
 
 cdir=${1:-"$(pwd)"}
 echo "Installing to ${cdir}"
@@ -13,16 +14,16 @@ mkdir -p ${CRAWLER_CHROME_DRIVER_PATH}
 mkdir -p ${CRAWLER_CHROME_PATH}
 mkdir -p ${CRAWLER_CHROME_PROFILE_PATH}
 
-# Chrome version 130; Also change version in run_consent_crawl.py if upgrading
+# Chrome version 131; Also change version in run_consent_crawl.py if upgrading
 cd /tmp/ || exit
-wget -q -O chrome-linux.zip 'https://www.googleapis.com/download/storage/v1/b/chromium-browser-snapshots/o/Linux_x64%2F1356007%2Fchrome-linux.zip?generation=1726513315891763&alt=media'
+wget -q -O chrome-linux.zip 'https://www.googleapis.com/download/storage/v1/b/chromium-browser-snapshots/o/Linux_x64%2F1368524%2Fchrome-linux.zip?generation=1728952252940053&alt=media'
 unzip -o chrome-linux.zip
-mv -f chrome-linux/* "${CRAWLER_CHROME_PATH}"
+rsync -arh --delete chrome-linux/* "${CRAWLER_CHROME_PATH}"
 
-wget -q -O chromedriver.zip 'https://www.googleapis.com/download/storage/v1/b/chromium-browser-snapshots/o/Linux_x64%2F1356007%2Fchromedriver_linux64.zip?generation=1726513319019044&alt=media'
+wget -q -O chromedriver.zip 'https://www.googleapis.com/download/storage/v1/b/chromium-browser-snapshots/o/Linux_x64%2F1368524%2Fchromedriver_linux64.zip?generation=1728952255946631&alt=media'
 
 unzip -o chromedriver.zip
-mv -f chromedriver_linux64/chromedriver "${CRAWLER_CHROME_DRIVER_PATH}"
+rsync -arh --delete chromedriver_linux64/chromedriver "${CRAWLER_CHROME_DRIVER_PATH}"
 
 rm /tmp/chrome-linux.zip
 rm /tmp/chromedriver.zip
