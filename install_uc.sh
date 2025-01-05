d198e2cbbe8d2054979753a05f532928cdd8d177
David Bernhard <dbernhard@ethz.ch>
2025-01-05 02:41:58 +0100

    Revert to old chromium version where the CookieBlock extension still works

--- install_uc.sh
+++ install_uc.sh
@@ -14,13 +14,13 @@ mkdir -p ${CRAWLER_CHROME_DRIVER_PATH}
 mkdir -p ${CRAWLER_CHROME_PATH}
 mkdir -p ${CRAWLER_CHROME_PROFILE_PATH}
 
-# Chrome version 131; Also change version in run_consent_crawl.py if upgrading
+# Chrome version 126; DO NOT UPGRADE, cookieblock does not support a higher version;  Also change version in run_consent_crawl.py if upgrading
 cd /tmp/ || exit
-wget -q -O chrome-linux.zip 'https://www.googleapis.com/download/storage/v1/b/chromium-browser-snapshots/o/Linux_x64%2F1368524%2Fchrome-linux.zip?generation=1728952252940053&alt=media'
+wget -q -O chrome-linux.zip 'https://www.googleapis.com/download/storage/v1/b/chromium-browser-snapshots/o/Linux_x64%2F1300319%2Fchrome-linux.zip?generation=1715640415104812&alt=media'
 unzip -o chrome-linux.zip
 rsync -arh --delete chrome-linux/* "${CRAWLER_CHROME_PATH}"
 
-wget -q -O chromedriver.zip 'https://www.googleapis.com/download/storage/v1/b/chromium-browser-snapshots/o/Linux_x64%2F1368524%2Fchromedriver_linux64.zip?generation=1728952255946631&alt=media'
+wget -q -O chromedriver.zip 'https://www.googleapis.com/download/storage/v1/b/chromium-browser-snapshots/o/Linux_x64%2F1300319%2Fchromedriver_linux64.zip?generation=1715640418543191&alt=media'
 
 unzip -o chromedriver.zip
 rsync -arh --delete chromedriver_linux64/chromedriver "${CRAWLER_CHROME_DRIVER_PATH}"
